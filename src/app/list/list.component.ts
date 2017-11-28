b84f6eb5d54c8a7732543551a04b299ceee6ac8a
Hamdi Chebbi <hamdi.chebbi@outlook.fr>
2017-11-28 21:48:05 +0100

    Map & Share
    Map + Partage

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { GeolocationService } from '../geolocation.service';
 import { Coffee } from '../../model/Coffee';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -11,12 +12,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ListComponent implements OnInit {
   // injecter la dataService
   list: [Coffee];
-  constructor(private data: DataService , private router: Router) { }
+  constructor(
+    private data: DataService,
+    private router: Router,
+    private geolocation: GeolocationService
+  ) { }
 
-  goDetail(coffee : Coffee){
+  goDetail(coffee: Coffee) {
     this.router.navigate(["/coffee", coffee._id])
   }
 
+  goMap(coffee: Coffee) {
+    // open Map Application ! just need to change the URL ( iOS , Android )
+    const mapUrl = this.geolocation.getMapLink(coffee.location);
+    location.href = mapUrl;
+  }
+
+  share(coffee: Coffee) {
+    const shareText = `J'étais au caffé, ${coffee.place} et je note ${coffee.rating} etoiles`;
+    if ('share' in navigator) {
+      (navigator as any).share({
+        title: coffee.name,
+        text: shareText,
+        url: window.location.href
+      }).then(() => console.log('shared')).catch(() => console.log('error sharing'));
+    } else {
+      const shareUrl = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+      location.href = shareUrl;
+    }
+  }
   ngOnInit() {
     //get the list
     this.data.getList(list => {
